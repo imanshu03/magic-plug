797a93c38d46aafedab4e54085917849d7d081ab
Imanshu Rathore <imanshurathore@gmail.com>
2024-01-21 16:58:40 +0530

    feat: integrated gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -228,10 +228,32 @@ export default async function RootLayout({
                 gtag("config", "G-X2L15MZPF3");
               `}
             </Script>
+            <Script id="gtm">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MRL34QC9');
+                `}
+            </Script>
           </>
         ) : null}
       </head>
       <body className="bg-app-bg w-screen h-auto font-manrope">
+        {process.env.ENVIRONMENT !== "development" ? (
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MRL34QC9"
+              height="0"
+              width="0"
+              style={{
+                display: "none",
+                visibility: "hidden",
+              }}
+            ></iframe>
+          </noscript>
+        ) : null}
         <ToastContainer className="text-dark-primary font-manrope" />
         <SmoothScrollWrapper linkData={linkData}>
           {children}
